5b919387e0f1f0b5f8e40b57a3731044e87f4822
azazellooy <khismatova_05@mail.ru>
2022-08-09 15:49:13 +0300

    first commit

---
+++ Lab1/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CleverensSoft.Lab1
+{//https://docs.microsoft.com/ru-ru/dotnet/api/system.threading.readerwriterlockslim?view=net-6.0
+    public static class Server
+    {
+        private static int _count;
+        private static ReaderWriterLockSlim _readerWriterLockSlim = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            _readerWriterLockSlim.EnterReadLock();
+            try
+            {
+                return _count;
+            }
+            finally
+            {
+                _readerWriterLockSlim.ExitReadLock();
+                Console.WriteLine("читатель дочитал");
+            }
+           
+        }
+
+        public static int AddToCount(int value)
+        {
+            _readerWriterLockSlim.EnterWriteLock();
+            try
+            {
+                return _count += value;
+            }
+            finally
+            {
+                _readerWriterLockSlim.ExitWriteLock();
+                Console.WriteLine("писатель дописал");
+            }
+        }
+    }
+}
+
